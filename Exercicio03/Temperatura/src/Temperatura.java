65132e56fd575e11cf13ded9f643df89b4be3950
Breno Alves <106405847+Balves129@users.noreply.github.com>
2024-12-29 10:36:30 -0300

    29/12/24

--- Exercicio03/Temperatura/src/Temperatura.java
+++ Exercicio03/Temperatura/src/Temperatura.java
@@ -1,17 +1,59 @@
 import java.util.Scanner;
+
 public class Temperatura {
     public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
-        
-        //Solicitar para que digite a temperatura em celsius
-        System.out.println("Digite a temperatura em Graus Celsius: ");
+        boolean continuar = true;
+
+        // Defini por padrão,a temperatura em Grau Celsius
+
+        System.out.println("Digite a temperatura em Celsius: ");
         double Celsius = scanner.nextDouble();
 
-        //Converter a temperatura para fahrenheit
-        double Fahrenheit = (Celsius * 1.8) + 32;
-        
-        //Mostrar a temperatura convertida para fahrenheit
-        System.out.printf("A temperatura de %.1f°C é %.1f°F%n", Celsius, Fahrenheit);
-    }
-}
+        while (continuar) {
+            // Mostrar as opções de conversão
+            System.out.println("\nEscolha a opção de conversão:");
+            System.out.println("1 -Para Fahrenheit");
+            System.out.println("2 -Para Réaumur");
+            System.out.println("3 -Para Kelvin");
+            System.out.println("4 - Sair");
+
+            int Opcao = scanner.nextInt();
+            switch (Opcao) {
+                case 1:
+                    // Conversão para Fahrenheit
+
+                    double Fahrenheit = (Celsius * 1.8) + 32;
+                    System.out.printf("Temperatura em Fahrenheit %.1f°F%n ", Fahrenheit);
+                    break;
+
+                case 2:
+                    // Conversão para Réaumur
+
+                    double Reaumur = (Celsius * 0.8);
+                    System.out.printf("Temperatura em Reaumur %.1f°Re%n", Reaumur);
+                    break;
+
+                case 3:
+                    // Conversão para Kelvin
+
+                    double Kelvin = Celsius + 273.15;
+                    System.out.printf("Temperatura em Kelvin %.1f°K%n", Kelvin);
+                    break;
+
+                case 4:
+                    continuar = false;
+                    break;
+                default:
+                    System.out.print("Opção Inválida. Selecione novamente.");
+
+            }
+            ;
+
+        }
+        ;
+
+        scanner.close();
+    };
+};
