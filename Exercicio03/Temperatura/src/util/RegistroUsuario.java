ba41d5559da3fdc479b7d19e94399e284b08bef8
Breno Alves <106405847+Balves129@users.noreply.github.com>
2024-12-30 18:56:11 -0300

    30/12/2024

---
+++ Exercicio03/Temperatura/src/util/RegistroUsuario.java
@@ -0,0 +1,16 @@
+package util;
+
+public class RegistroUsuario {
+    private String nome;
+
+    // Construtor para inicializar o nome
+    public RegistroUsuario(String nome) {
+        this.nome = nome;
+    }
+
+    // Getter para acessar o nome
+    public String getNome() {
+        return nome;
+    }
+}
+
